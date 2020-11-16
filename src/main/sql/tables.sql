fad44e0722b68b48696947f6d6adc03641d5ca4a
Anton Bulgakov <anton.bulgakov@dataart.com>
2020-11-16 14:14:22 +0200

    (ABI-1) Changed table's fields naming. Modified table 'users'

--- src/main/sql/tables.sql
+++ src/main/sql/tables.sql
@@ -1,50 +1,51 @@
 -- psql -U dbadmin -d playme
 
 CREATE TABLE roles(
-	role_id varchar(36) NOT NULL PRIMARY KEY,
-	role_name varchar(64) NOT NULL UNIQUE
+	id varchar(36) NOT NULL PRIMARY KEY,
+	name varchar(64) NOT NULL UNIQUE
 );
 
 CREATE TABLE statuses(
-	status_id varchar(36) NOT NULL PRIMARY KEY,
-	status_name varchar(64) NOT NULL UNIQUE
+	id varchar(36) NOT NULL PRIMARY KEY,
+	name varchar(64) NOT NULL UNIQUE
 );
 
 CREATE TABLE users(
 	user_id varchar(36) NOT NULL PRIMARY KEY,
-	user_login varchar(64) NOT NULL UNIQUE,
-	user_password varchar(64) NOT NULL,
-	user_email varchar(320) NOT NULL UNIQUE,
-	user_birthdate date NOT NULL,
-	user_name varchar(64) NOT NULL,
+	login varchar(64) NOT NULL UNIQUE,
+	password varchar(64) NOT NULL,
+    email varchar(320) NOT NULL UNIQUE,
+	birthdate date NOT NULL,
+	first_name varchar(64) NOT NULL,
+	last_name varchar(64) NOT NULL,
 	role_id varchar(36) NOT NULL,
 	status_id varchar(36) NOT NULL,
 	creation_date date not null,
 	modification_date date not null,
-	FOREIGN KEY(role_id) REFERENCES roles(role_id),
-	FOREIGN KEY(status_id) REFERENCES statuses(status_id)
+	FOREIGN KEY(role_id) REFERENCES roles(id),
+	FOREIGN KEY(status_id) REFERENCES statuses(id)
 );
 
 CREATE TABLE musicians(
-	musician_id varchar(36) NOT NULL PRIMARY KEY,
-	musician_nickname varchar(64) NOT NULL,
+	id varchar(36) NOT NULL PRIMARY KEY,
+	nickname varchar(64) NOT NULL,
 	user_id varchar(36) NOT NULL UNIQUE,
 	band_id varchar(36)
 );
 
 CREATE TABLE bands(
-	band_id varchar(36) NOT NULL PRIMARY KEY,
-	band_name varchar(64) NOT NULL UNIQUE,
-	band_creation_date date NOT NULL,
-	band_leader_id varchar(36) NOT NULL,
-	FOREIGN KEY(band_leader_id) REFERENCES users(user_id)
+	id varchar(36) NOT NULL PRIMARY KEY,
+	name varchar(64) NOT NULL UNIQUE,
+	creation_date date NOT NULL,
+	leader_id varchar(36) NOT NULL,
+	FOREIGN KEY(leader_id) REFERENCES users(id)
 );
 
-ALTER TABLE musicians ADD FOREIGN KEY(band_id) REFERENCES bands(band_id);
+ALTER TABLE musicians ADD FOREIGN KEY(band_id) REFERENCES bands(id);
 
 CREATE TABLE skills(
-	skill_id varchar(36) NOT NULL PRIMARY KEY,
-	role_name varchar(64) NOT NULL UNIQUE
+	id varchar(36) NOT NULL PRIMARY KEY,
+	name varchar(64) NOT NULL UNIQUE
 );
 
 CREATE TABLE skills_musicians(
@@ -52,23 +53,23 @@ CREATE TABLE skills_musicians(
 	musician_id varchar(36) NOT NULL,
 	skill_level varchar(64) NOT NULL,
 	CONSTRAINT skills_musicians_primary_key PRIMARY KEY (skill_id, musician_id),
-	FOREIGN KEY(skill_id) REFERENCES skills(skill_id),
-	FOREIGN KEY(musician_id) REFERENCES musicians(musician_id)
+	FOREIGN KEY(skill_id) REFERENCES skills(id),
+	FOREIGN KEY(musician_id) REFERENCES musicians(id)
 );
 
 CREATE TABLE repetitions(
-	repetition_id varchar(36) NOT NULL PRIMARY KEY,
-	repetition_start_datetime date NOT NULL,
-	repetition_finish_datetime date,
-	repetition_description varchar,
-	repetition_creator_id varchar(36) NOT NULL,
-	FOREIGN KEY(repetition_creator_id) REFERENCES musicians(musician_id)
+	id varchar(36) NOT NULL PRIMARY KEY,
+	start_datetime date NOT NULL,
+	finish_datetime date,
+	description varchar,
+	creator_id varchar(36) NOT NULL,
+	FOREIGN KEY(creator_id) REFERENCES musicians(id)
 );
 
 CREATE TABLE repetitions_musicians(
 	repetition_id varchar(36) NOT NULL,
 	musician_id varchar(36) NOT NULL,
 	approved boolean NOT NULL DEFAULT FALSE,
-	FOREIGN KEY(repetition_id) REFERENCES repetitions(repetition_id),
-	FOREIGN KEY(musician_id) REFERENCES musicians(musician_id)
+	FOREIGN KEY(repetition_id) REFERENCES repetitions(id),
+	FOREIGN KEY(musician_id) REFERENCES musicians(id)
 );
